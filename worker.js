0fdfba8af79a0a8c84cdc1a65da58b3b524fa641
kenn <63381721+liuing-org@users.noreply.github.com>
2025-12-04 09:57:21 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -113,6 +113,7 @@ export default {
   <body>
     <div class="message">
       <!-- IT请假啦!<br>有问题请在<br>Teams上留言 -->
+      <!-- IT出差啦!<br>12/8 - 1/9<br>有问题请在<br>Teams上留言 -->
       IT 正在打工中!
     </div>
   </body>
